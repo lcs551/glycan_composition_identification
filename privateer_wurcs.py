de036a6780fd8500439f8667edc4a25ffdd66a9d
Lucy Schofield <lcs551@york.ac.uk>
2024-01-25 09:51:19 +0000

    Changed to use splitlines()

--- privateer_wurcs.py
+++ privateer_wurcs.py
@@ -3,7 +3,6 @@
 import os
 from privateer import privateer_core as pvt
 import pandas as pd
-from io import StringIO
 
 directory = "/Users/lucyschofield/phd/xhpi/pdb_files"
 output_csv_file_path = "WURCS_privateer_output.csv"
@@ -11,10 +10,10 @@ output_csv_file_path = "WURCS_privateer_output.csv"
 def get_sugar_id (data_io, output_csv_file_path, file_name):
     ids = []
     wurcs_list = []
-    lines = data_io.readlines()
-    for i in range(1, len(lines), 2):  # Start from the second line, skipping the first line
-        ids.append(lines[i].strip())
-        wurcs_list.append(lines[i + 1].strip())
+    
+    for i in range(1, len(data_io), 2):  # Start from the second line, skipping the first line
+        ids.append(data_io[i].strip())
+        wurcs_list.append(data_io[i + 1].strip())
 
     df = pd.DataFrame({"ID": ids, "WURCS": wurcs_list})
     df['TSChainId'] = df['ID'].apply(lambda x: x.split('_')[0][-1] if x.split('_')[0] else None)
@@ -26,8 +25,8 @@ def get_sugar_id (data_io, output_csv_file_path, file_name):
 def get_wurcs (file_path, output_csv_file_path):
     file_name = os.path.splitext(os.path.basename(file_path))[0]
     totalWURCS = pvt.print_wurcs(file_path)
-    data_io = StringIO(totalWURCS)
-    get_sugar_id(data_io, output_csv_file_path, file_name)
+    wurcs_list = totalWURCS.splitlines()
+    get_sugar_id(wurcs_list, output_csv_file_path, file_name)
 
 if __name__ == "__main__":
     # List all files in the directory with a ".pdb" extension
